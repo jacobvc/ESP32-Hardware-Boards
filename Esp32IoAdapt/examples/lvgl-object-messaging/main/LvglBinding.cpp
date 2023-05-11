0586f83c518c5c3288fea3de4fc00c1cffc4193c
jacobvc <jacobvc@users.noreply.github.com>
2023-05-11 11:42:22 -0500

    Updated ui_bind.py output

--- Esp32IoAdapt/examples/lvgl-object-messaging/main/LvglBinding.cpp
+++ Esp32IoAdapt/examples/lvgl-object-messaging/main/LvglBinding.cpp
@@ -1,4 +1,4 @@
-/** LvglBinding.cpp   Generated 2023-05-08
+/** LvglBinding.cpp   Generated 2023-05-11
  *
  * Generated using ESP-Object-Messaging: tools/ui_bind.py. Do not edit manually.
  * 
@@ -12,10 +12,10 @@ void LvglBindingInit(LvglHost& host)
     host.AddProducer("sample", ui_btnSample, BUTTON_CT, LV_EVENT_CLICKED);
     host.AddConsumer("sample", ui_btnSample, BUTTON_CT);
     host.AddConsumer("pressure", ui_lblPressure, LABEL_CT);
-    host.AddProducer("zoomy", ui_sldZoomY, SLIDER_CT, LV_EVENT_VALUE_CHANGED);
-    host.AddConsumer("zoomy", ui_sldZoomY, SLIDER_CT);
     host.AddProducer("zoomx", ui_arcZoomX, ARC_CT, LV_EVENT_VALUE_CHANGED);
     host.AddConsumer("zoomx", ui_arcZoomX, ARC_CT);
+    host.AddProducer("zoomy", ui_sldZoomY, SLIDER_CT, LV_EVENT_VALUE_CHANGED);
+    host.AddConsumer("zoomy", ui_sldZoomY, SLIDER_CT);
     host.AddProducer("schedule", ui_calSchedule, CALENDAR_CT, LV_EVENT_VALUE_CHANGED);
     host.AddConsumer("schedule", ui_calSchedule, CALENDAR_CT);
 }
